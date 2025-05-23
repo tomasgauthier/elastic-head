565510f2c1e7fd57505a867ee8e15b2c1c75768b
Tomás Gauthier <tomas.gauthier.a@gmail.com>
2025-05-22 23:04:13 -0400

    Update configuration for deployment

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000']
-    }
-  },
+  }
 }
 
 module.exports = nextConfig 
